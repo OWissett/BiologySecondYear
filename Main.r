3c3c6f300cb49e8dc2ea876bdea2357cba03ff89
ThatOldGuy <wissettol@gmail.com>
2018-11-12 13:23:34 +0000

    Add files via upload

---
+++ Main.r
@@ -0,0 +1,41 @@
+##############################
+##Cell Cycle Lab Report Code##
+##############################
+
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(gridExtra)
+
+##############################
+
+## LOAD DATA
+my.Data <- function(){
+  Mdata <- list()
+  Mdata[[1]] <- read.csv("MyData.csv", header = T, sep=",")
+  Mdata[[2]] <- read.csv("ClassData.csv", header= T, sep=",")
+  return(Mdata)
+}
+
+## DRAW GRAPH
+my.Draw <- function(s){
+  dat <- my.Data()
+  plots <- list()
+  for(i in 1:2){
+    plots[[i]] <- ggplot(data = dat[[i]], aes(Time, SI)) + 
+      geom_point() +
+      geom_smooth(method="loess", span=s) + 
+      xlab("Time (Minutes)") + 
+      ylab("Septation Index (%)") +
+      scale_colour_brewer(palette="Paired")+
+      ggtitle("Septation over time during synchronous mitosis in S. Pombe")
+  }
+  grid.arrange(plots[[1]], plots[[2]], nrow=2, ncol=1)
+}
+
+
+
+
+
+
+
