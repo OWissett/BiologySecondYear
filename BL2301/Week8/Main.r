01b76054a0ab8c212740537699084c4a0bb86ad8
Oliver Wissett <ow23@st-andrews.ac.uk>
2018-11-14 11:53:53 +0000

    Added Comments to the code

--- BL2301/Week8/Main.r
+++ BL2301/Week8/Main.r
@@ -2,6 +2,7 @@
 ##Cell Cycle Lab Report Code##
 ##############################
 
+##Inialise the Appropriate libraries
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
@@ -11,6 +12,9 @@ library(plotly)
 ##############################
 
 ## LOAD DATA
+##INPUT:  2 CSV files loaded from the working directory
+##OUTPUT: Mdata, a list of CSV data frames with 2 columns.
+
 my.Data <- function(){
   Mdata <- list()
   Mdata[[1]] <- read.csv("MyData.csv", header = T, sep=",")
@@ -19,9 +23,17 @@ my.Data <- function(){
 }
 
 ## DRAW GRAPH
+##INPUT:  smMeth, the Method for line estimation; smForm, the formula used
+##        creation of the line; smFam, defines the family of the type of line estimator used.
+##OUTPUT: a graphics grid that contains 2 ggplots of the data obtain in my.Data().
+
 my.Draw <- function(smMeth, smForm, smFam){
+  ##Creates local variable from my.Data()
   dat <- my.Data()
+  ##Defines "plots" as a list, this allows the variable to be used in the loop
   plots <- list()
+  
+  ##Loop cycles 2 times, once for each plot. The loop creates a list of ggplots
   for(i in 1:2){
     plots[[i]] <- ggplot(data = dat[[i]], aes(Time, SI)) + 
       geom_point() +
@@ -31,6 +43,7 @@ my.Draw <- function(smMeth, smForm, smFam){
       scale_colour_brewer(palette="Paired")+
       ggtitle("Septation over time during synchronous mitosis in S. Pombe")
   }
+  ##Each item within plots[] is diplayed on the graphics grid
   grid.arrange(plots[[1]], plots[[2]], nrow=2, ncol=1)
 }
 
