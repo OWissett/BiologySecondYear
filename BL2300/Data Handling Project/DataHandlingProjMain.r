aee862cbe7cfd4d360747d8a71607fa333ac14da
Oliver Wissett <ow23@st-andrews.ac.uk>
2018-11-20 17:42:24 +0000

    my.PrintTextToPlot function Added, my.ModelPlot modified to add AIC and Call function, Diary Added

--- BL2300/Data Handling Project/DataHandlingProjMain.r
+++ BL2300/Data Handling Project/DataHandlingProjMain.r
@@ -34,7 +34,7 @@ fdat <- subset(dat, dat$sex == "F")
 ###################################
 
 ##Purpose: Defines plausible biological GLMs
-##Output: List of Models
+##Output: List of 12 Models
 my.Models <- function(){
   
   model <- list()
@@ -107,6 +107,14 @@ my.Models <- function(){
   ##Best Models: 1, 5, 9. (Mixed. Males, Females)
 }
 
+my.PrintTextToPlot <- function(my_text){
+  par(mar=c(0,0,0,0))
+  plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+  text(x = 0.5, y = 0.5, my_text, 
+       cex = 1.6, col = "black")
+  par(mar = c(5, 4, 4, 2) + 0.1)
+}
+
 ###################################
 ##    USER CALL FUNCTIONS        ##
 ###################################
@@ -134,9 +142,15 @@ my.Summary <- function(m, n){
 my.ModelPlot <- function(m, n){
   model <- my.Models()
   for(i in m:n){
-    png(filename = paste("AnalysisPlots/Graph", toString(i), ".png"))
-    par(mfrow=c(2,2))
+    AIC_text <- toString(AIC(model[[i]]))
+    Form_text <- toString(model[[i]][["call"]]) ## FIX THIS: Aim: output the call for model[[i]]
+    png(filename = paste("Analysis Plots/Graph", toString(i), ".png"),
+        width=1920, 
+        height=1080)
+    par(mfrow=c(2,3))
+    my.PrintTextToPlot(paste("AIC: ", sum_text, "/n Formula: ", Form_text))
     plot(model[[i]])
+    hist(resid(model[[i]]), main = paste("Histogram of Residues for Model: ", toString(i)), xlab="Residue Value")
     dev.off()
   }
 }
@@ -150,39 +164,64 @@ my.Graph <- function(){
   
   plots <- list()
   pred <- list()
-  
   mods <- list(my.Models()[[1]], my.Models()[[5]], my.Models()[[9]])
   
-  
-  ##Mixed Predictor
+  ##Mixed Population Predictor
   pred[[1]] <- data.frame(sysbp_pred = predict(mods[[1]]), age=dat$age, smoke=dat$smoke)
   
-  ##Male Predictor
+  ##Male Population Predictor
   pred[[2]] <- data.frame(sysbp_pred = predict(mods[[2]]), age=mdat$age, smoke=mdat$smoke)
   
-  ##Female Predictor
+  ##Female Population Predictor
   pred[[3]] <- data.frame(sysbp_pred = predict(mods[[3]]), age=fdat$age, smoke=fdat$smoke)
-
   
-  ##Mixed
-  plots[[1]] <- ggplot(dat, aes(age, sysbp, colour=smoke)) + 
+  ##Mixed Population graph showing Systolic BP against 
+  plots[[1]] <- ggplot(dat, aes(age, sysbp, shape=smoke, colour=dat$cholest)) + 
     geom_point() +
-    geom_line(data=pred[[1]], aes(age, sysbp_pred))
+    geom_line(data=pred[[1]], aes(age, sysbp_pred)) +
+    ggtitle("Effect of Age, Cholesterol and Smoking on Systolic Blood Pressure", "Mixed Sex")
   
   ##Males
-  plots[[2]] <- ggplot(mdat, aes(age, sysbp, colour=smoke)) + 
+  plots[[2]] <- ggplot(mdat, aes(age, sysbp, shape=smoke, colour=mdat$cholest)) + 
     geom_point() +
-    geom_line(data=pred[[2]], aes(age, sysbp_pred))
+    geom_line(data=pred[[2]], aes(age, sysbp_pred)) +
+    ggtitle("Effect of Age, Cholesterol and Smoking on Systolic Blood Pressure", "Males")
   
   ##Females
-  plots[[3]] <- ggplot(fdat, aes(age, sysbp, colour=smoke)) + 
+  plots[[3]] <- ggplot(fdat, aes(age, sysbp, shape=smoke, colour=fdat$cholest)) + 
     geom_point() +
-    geom_line(data=pred[[3]], aes(age, sysbp_pred))
+    geom_line(data=pred[[3]], aes(age, sysbp_pred)) +
+    ggtitle("Effect of Age, Cholesterol and Smoking on Systolic Blood Pressure", "Females")
+  
   
   grid.arrange(plots[[1]])
   grid.arrange(plots[[2]], plots[[3]], ncol=2)
+  
 }
 
+####################################
+##     NOTES  / DIARY             ##
+####################################
+##
+## 20/11/18 - Day 1
+##
+## The best models were found to be 1, 5, 9. These models do not include
+## an interaction between age and smoking. I found that I have been able to
+## show 4 independent variable using, x axis = age, shape = smoking + gender
+## and colour gradient = cholesterol. I am unsure whether this is stistically 
+## correct. NOTE TO SELF: Ask Prof. Creswell during lab whether I can add 
+## cholesterol to the graph using the colour function, even if it isnt included 
+## as a covariate within the model or whether it must be added to the model and
+## interactions must be tested or whether I should leave it out as it is not 
+## necessary for the assignment...
+##
+## To Do: * Finish the labelling of my.Graph's graphs and the legends
+##        * Solve the issue with my.ModelPlot, I want to be able to print the call
+##          formula from the summary data of the model[[i]], this is not super 
+##          important as it could be done manually, but this is a pain in the bum.
+##        * Imrpove visual style of the document and clean up code and files.
+## 
+##  
 
 
 
