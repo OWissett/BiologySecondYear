5c4790eebf0e977ef178461073e184014aa5d824
Oliver Wissett <ow23@st-andrews.ac.uk>
2018-11-21 16:17:35 +0000

    Completed Analysis Images for models 1-12

--- BL2300/Data Handling Project/DataHandlingProjMain.r
+++ BL2300/Data Handling Project/DataHandlingProjMain.r
@@ -14,7 +14,7 @@
 
 ##ggplot requires dplyr and pillar, some university workstations do not 
 ##automatically load dependencies for a packages. This code has been 
-##add so that the deployability of the code is increased.
+##added so that the deployability of the code is increased.
 library("dplyr")
 library("pillar")
 library("ggplot2")
@@ -28,6 +28,10 @@ dat <- read.csv("dataset.csv", header=T, sep=",")
 mdat <- subset(dat, dat$sex == "M")
 fdat <- subset(dat, dat$sex == "F")
 
+dat$class[dat$smoke=="N" & dat$sex=="M"] <- "1" ##Male non smokers
+dat$class[dat$smoke=="Y" & dat$sex=="M"] <- "2" ##Male smokers
+dat$class[dat$smoke=="N" & dat$sex=="F"] <- "3" ##Female non smokers
+dat$class[dat$smoke=="Y" & dat$sex=="F"] <- "4" ##Female smokers
 
 ###################################
 ##     AUTOCALL FUNCTIONS        ##
@@ -46,7 +50,7 @@ my.Models <- function(){
   
   ## Sysbp Depends on Smoking and Age, with no iteraction
   ## AIC: 705 
-  model[[1]] <- glm(sysbp~age+smoke ,data=dat)
+  model[[1]] <- glm(sysbp~age+smoke, data=dat)
   
   ## Sysbp depends on smoking and age, with interaction between age and smoking
   ## AIC: 707.8 
@@ -66,7 +70,7 @@ my.Models <- function(){
   
   ## Sysbp Depends on Smoking and Age, with no iteraction
   ## AIC: 344.1
-  model[[5]] <- glm(sysbp~age+smoke ,data=mdat)
+  model[[5]] <- glm(sysbp~age+smoke, data=mdat)
   
   ## Sysbp depends on smoking and age, with interaction between age and smoking
   ## AIC: 345
@@ -86,7 +90,7 @@ my.Models <- function(){
   
   ## Sysbp Depends on Smoking and Age, with no iteraction
   ## AIC: 330.8
-  model[[9]] <- glm(sysbp~age+smoke ,data=fdat)
+  model[[9]] <- glm(sysbp~age+smoke, data=fdat)
   
   ## Sysbp depends on smoking and age, with interaction between age and smoking
   ## AIC: 332.8
@@ -111,7 +115,7 @@ my.PrintTextToPlot <- function(my_text){
   par(mar=c(0,0,0,0))
   plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
   text(x = 0.5, y = 0.5, my_text, 
-       cex = 1.6, col = "black")
+       cex = 2, col = "black")
   par(mar = c(5, 4, 4, 2) + 0.1)
 }
 
@@ -136,23 +140,32 @@ my.Summary <- function(m, n){
 
 ##Purpose: Creates summary plots for the models
 ##INPUT:   m, the lower index number for the model to be summarised; n, the upper
-##         index number for the model to be summarised. Values must be integers.
+##         index number for the model to be summarised. Values must be integers
+##         MODE: 1 - Single Test, and will out test to Plot. 0 - List Test, and will ouput PNG file
 ##OUTPUT:  Plots of models outputted to graphics
 
-my.ModelPlot <- function(m, n){
-  model <- my.Models()
-  for(i in m:n){
-    AIC_text <- toString(AIC(model[[i]]))
-    Form_text <- toString(model[[i]][["call"]]) ## FIX THIS: Aim: output the call for model[[i]]
-    png(filename = paste("Analysis Plots/Graph", toString(i), ".png"),
-        width=1920, 
-        height=1080)
-    par(mfrow=c(2,3))
-    my.PrintTextToPlot(paste("AIC: ", sum_text, "/n Formula: ", Form_text))
-    plot(model[[i]])
-    hist(resid(model[[i]]), main = paste("Histogram of Residues for Model: ", toString(i)), xlab="Residue Value")
-    dev.off()
-  }
+my.ModelPlot <- function(m, n, MODE, model){
+  if(MODE==1){
+    AIC_text <- toString(AIC(model))
+    Form_text <- toString(model[["call"]])
+    par(mfrow=c(3,2))
+    plot(model)
+    my.PrintTextToPlot(paste("AIC: ", AIC_text, "\n Formula:\n ", Form_text))
+    hist(resid(model), main = "Histogram of Residues for Model", xlab="Residue Value")
+  }else if(MODE==0 & isNumeric(m) & isNumeric(n) & length(model)>1){
+    for(i in m:n){
+      AIC_text <- toString(AIC(model[[i]]))
+      Form_text <- toString(model[[i]][["call"]])
+      png(filename = paste("Analysis Plots/Graph", toString(i), ".png"),
+          width=1920, 
+          height=1080)
+      par(mfrow=c(3,2))
+      plot(model[[i]])
+      my.PrintTextToPlot(paste("AIC: ", AIC_text, "\n Formula: ", Form_text))
+      hist(resid(model[[i]]), main = paste("Histogram of Residues for Model: ", toString(i)), xlab="Residue Value")
+      dev.off()
+    }
+  }else{return("Enter Valid Parameters: 0 - List Test Mode, 1 - Single Test Mode")}
 }
 
 ##Purpose: Create plots the different data with Regression lines plotted
@@ -165,9 +178,9 @@ my.Graph <- function(){
   plots <- list()
   pred <- list()
   mods <- list(my.Models()[[1]], my.Models()[[5]], my.Models()[[9]])
-  
+
   ##Mixed Population Predictor
-  pred[[1]] <- data.frame(sysbp_pred = predict(mods[[1]]), age=dat$age, smoke=dat$smoke)
+  pred[[1]] <- data.frame(sysbp_pred = predict(mods[[1]]), age=dat$age, smoke=dat$smoke, sex=dat$sex)
   
   ##Male Population Predictor
   pred[[2]] <- data.frame(sysbp_pred = predict(mods[[2]]), age=mdat$age, smoke=mdat$smoke)
@@ -176,19 +189,19 @@ my.Graph <- function(){
   pred[[3]] <- data.frame(sysbp_pred = predict(mods[[3]]), age=fdat$age, smoke=fdat$smoke)
   
   ##Mixed Population graph showing Systolic BP against 
-  plots[[1]] <- ggplot(dat, aes(age, sysbp, shape=smoke, colour=dat$cholest)) + 
+  plots[[1]] <- ggplot(dat, aes(age, sysbp, shape=dat$class, colour=dat$sex)) + 
     geom_point() +
-    geom_line(data=pred[[1]], aes(age, sysbp_pred)) +
+    geom_line(data=pred[[1]], aes(age, sysbp_pred, colour=sex)) +
     ggtitle("Effect of Age, Cholesterol and Smoking on Systolic Blood Pressure", "Mixed Sex")
   
   ##Males
-  plots[[2]] <- ggplot(mdat, aes(age, sysbp, shape=smoke, colour=mdat$cholest)) + 
+  plots[[2]] <- ggplot(mdat, aes(age, sysbp, shape=smoke, colour=mdat$smoke)) + 
     geom_point() +
     geom_line(data=pred[[2]], aes(age, sysbp_pred)) +
     ggtitle("Effect of Age, Cholesterol and Smoking on Systolic Blood Pressure", "Males")
   
   ##Females
-  plots[[3]] <- ggplot(fdat, aes(age, sysbp, shape=smoke, colour=fdat$cholest)) + 
+  plots[[3]] <- ggplot(fdat, aes(age, sysbp, shape=smoke, colour=fdat$smoke)) + 
     geom_point() +
     geom_line(data=pred[[3]], aes(age, sysbp_pred)) +
     ggtitle("Effect of Age, Cholesterol and Smoking on Systolic Blood Pressure", "Females")
@@ -221,7 +234,13 @@ my.Graph <- function(){
 ##          important as it could be done manually, but this is a pain in the bum.
 ##        * Imrpove visual style of the document and clean up code and files.
 ## 
-##  
+##  21/11/18
+##
+## Fixed my.ModelPlot issue with Formula output. Modified the my.ModelPlot function 
+## allowing it to act on lists and an singlular models.
+## 
+## Currently investigating the affect of including cholesterol in the model. 
+## 
 
 
 
